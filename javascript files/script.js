120f2252623dc48b72a0d42803d2165f3053fa6d
Roy Shanto <122017004+royshanto71@users.noreply.github.com>
2025-09-06 15:14:43 +0600

    Refactor event listeners for heart, copy, and call buttons; improve code organization

--- javascript files/script.js
+++ javascript files/script.js
@@ -2,14 +2,34 @@ let heartCount = parseInt(document.getElementById("heart-count").innerText);
 let coinCount = parseInt(document.getElementById("coin-count").innerText);
 let copyCount = parseInt(document.getElementById("copy-count").innerText);
 
-// console.log(heartCount, coinCount, copyCount);
-
+// heart counter
 let heartBtns = document.getElementsByClassName("heart-button");
-// console.log(heartBtns);
 for (let btn of heartBtns) {
   btn.addEventListener("click", function () {
-    // console.log(btn);
     heartCount = heartCount + 1;
     document.getElementById("heart-count").innerText = heartCount;
   });
 }
+
+// copy counter
+let copyBtns = document.getElementsByClassName("copy-button");
+for (let btn of copyBtns) {
+  btn.addEventListener("click", function () {
+    copyCount = copyCount + 1;
+    document.getElementById("copy-count").innerText = copyCount;
+  });
+}
+
+//call feature
+let callBtns = document.getElementsByClassName("call-button");
+for (let btn of callBtns) {
+  btn.addEventListener("click", function () {
+    callCount = callCount + 1;
+    document.getElementById("call-count").innerText = callCount;
+  });
+}
+
+//clear history
+document.getElementById("history-clear").addEventListener("click", function () {
+  document.getElementById("history-container").innerHTML = "";
+});
